f1331f7495be102efbae156bde36ec8be3213603
Harry Braviner <harry.braviner@gmail.com>
2015-01-22 12:17:36 +0000

    Plot 1 partly working
    Plot1.r now produces the required plot, but does not output it to
    a PNG file yet.

--- plot1.R
+++ plot1.R
@@ -11,12 +11,22 @@ if(!file.exists(summaryDataFilename) | !file.exists(codeDataFilename)){
                 dataURL, sep=" "))
     download.file(, method = "wget")
   } else {
-    print(paste("Found file", zipFilename, "locally, will use this.", sep=" "));
+    print("Found file exdata-data-NEI_data.zip locally, will use this.");
   }
   unzip(zipFilename, exdir = WD)
 } else {
-  print(paste("Found files", summaryDataFilename, "and",
-              codeDataFilename, "locally, will use them.", sep=" "))
+  print("Found files summarySCC_PM25.rds and Source_Classification_Code.rds locally, will use them.")
 }
 
+print("Reading in data (may take some time)")
+NEI <- readRDS(summaryDataFilename)
 
+print("Plotting total emissions vs year")
+EvsY <- tapply(NEI$Emissions, NEI$year, FUN = sum)
+plot(names(EvsY), EvsY/(1e6), xlab = "Year",
+     ylab = "PM2.5 emission / Megatons", type = "b", pch = 19, axes=F,
+     main = "Total PM2.5 emissions per year from all sources\nin the United States")
+axis(side = 1, at = unique(NEI$year))
+axis(side = 2, at = floor(seq(from = ceiling(range(EvsY)[1]/1e6-1), to = floor(range(EvsY)[2]/1e6+1), length=6)))
+
+# FIXME - code to do output to a file
